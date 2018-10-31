649ba1e565e1d8b92fef6bbf40306b9e882b068a
zhuxiankang <673191402@qq.com>
2018-10-31 09:35:45 +0800

    feat(二分查找):

--- src/BinSearch.js
+++ src/BinSearch.js
@@ -40,8 +40,6 @@ function binSearch(arr, data) {
   while(lowerBound <= upperBound) {
     let mid = Math.floor((upperBound + lowerBound) / 2)
 
-    console.log(mid)
-
     if(arr[mid] < data) {
       lowerBound = mid + 1
     } else if(arr[mid] > data) {
@@ -84,3 +82,101 @@ let arr = [65,1,6,3,8,2,9,1,11,22,33,7,89,65,37,215,13,8,897,23,55,55,55,78,899,
 insertionSort(arr)
 console.log(arr)
 console.log(binSearch(arr, 1))
+
+
+/** 
+ * 
+计算重复数据：
+
+当binSearch() 函数找到某个值时，如果在数据集中还有其他相同的值出现，那么该函数会定位在类似值的附近。
+
+换句话说，其他相同的值可能会出现已找到值的左边或右边。
+
+所以一个统计重复值的函数要怎么做才能确保统计到了数据集中出现的所有重复的值呢？
+
+最简单的解决方案是写两个循环，两个都同时对数据集向下遍历，或者向左遍历，统计重复次数；然后，向上或向右遍历，统计重复次数。
+
+ */
+
+function binRepeatCount(arr, data) {
+  let count = 0
+
+  let position = binSearch(arr, data)
+
+  if(position > -1) {
+    ++ count
+    
+    let i = position
+
+    while(arr[++ position] === data) {
+      ++ count
+    } 
+
+    while(arr[-- i] === data)  {
+      ++ count
+    }
+  }
+
+  return count
+
+}
+
+
+console.log(binRepeatCount(arr, 65))
+
+
+/** 
+ * 
+查找文本数据：
+
+
+
+ */
+
+
+ let strArr = `The nationalism of Hamilton was undemocratic. The democracy of Jefferson was
+ in the beginning, provincial. The historic mission of uniting nationalism and
+ democracy was in the course of time given to new leaders from a region beyond
+ the mountains, peopled by men and women from all sections and free from those
+ state traditions which ran back to the early days of colonization. The voice
+ of the democratic nationalism nourished in the West was heard when Clay of
+ Kentucky advocated his American system of protection for industries; when
+ Jackson of Tennessee condemned nullification in a ringing proclamation that
+ has taken its place among the great American state papers; and when Lincoln
+ of Illinois, in a fateful hour, called upon a bewildered people to meet the
+ supreme test whether this was a nation destined to survive or to perish. And
+ it will be remembered that Lincoln's party chose for its banner that earlier
+ device--Republican--which Jefferson had made a sign of power. The "rail splitter"
+ from Illinois united the nationalism of Hamilton with the democracy of Jefferson,
+ and his appeal was clothed in the simple language of the people, not in the
+ sonorous rhetoric which Webster learned in the schools.`.split(' ')
+
+// 注意这个数组里没有区分标点符号
+console.log(strArr)
+
+
+/** 
+ * @Author: zhuxiankang 
+ * @Date:   2018-10-30 08:54:57  
+ * @Desc:   线性查找数组的索引 
+ * @Parm:    
+ */
+function findIndex(arr, data) {
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i] === data) {
+      return i
+    }
+  }
+  return -1
+}
+
+console.time('线性查找耗时')
+console.log(findIndex(strArr, 'rhetoric'))
+console.timeEnd('线性查找耗时')
+
+insertionSort(strArr)
+console.log(strArr)
+
+console.time('二分查找耗时')
+console.log(binSearch(strArr, 'rhetoric'))
+console.timeEnd('二分查找耗时')
