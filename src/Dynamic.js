825ce888293bbeab201a828e0855180d3d716f31
zhuxiankang <673191402@qq.com>
2018-11-02 09:43:53 +0800

    feat(动态规划):

--- src/Dynamic.js
+++ src/Dynamic.js
@@ -91,3 +91,89 @@ console.log(recursionFibonacci(8))
  // 递归耗时: 2.30419921875ms
  // 动态规划耗时: 0.22900390625ms
 
+
+
+
+ /*
+
+寻找最长公共子串：例如，在单词“raven”和“havoc”中，最长的公共子串是“av”。
+
+动态规划是适合解决这个问题的方案。这个算法使用一个二维数组存储两个字符串相同位置的字符比较结果。
+
+初始化时，该数组的每一个元素被设置为0。
+
+每次在这两个数组的相同位置发现了匹配，就将数组对应行和列的元素加1，否则保持为0。
+
+按照这种方式，一个变量会持续记录下找到了多少个匹配项。
+
+当算法执行完毕时，这个变量会结合一个索引变量来获得最长公共子串。
+
+*/
+
+
+function lcs(word1, word2) {
+  let max = 0
+  let index = 0
+
+  let lcsArr = new Array(word1.length + 1)
+
+  // 动态开辟二维数组大小
+  for(let i=0; i<=word1.length + 1; i++) {
+    for(let j=0; j<=word2.length + 1; j++) {
+      lcsArr[i] = new Array(word2.length + 1)
+    }
+  }
+
+
+  for(let i=0; i<=word1.length; i++) {
+    for(let j=0; j<=word2.length; j++) {
+
+      // 默认矩阵的横竖起始位置都是0
+      if(i==0 || j==0) {
+        lcsArr[i][j] = 0
+        console.log(`${i} - ${j} : `,  lcsArr[i][j])
+        continue
+      } 
+
+      // 如果当前位置的两个字符比对相等，则匹配数等于各自前一个字符匹配数 + 1
+      if(word1[i-1] === word2[j-1]) {
+        lcsArr[i][j] = lcsArr[i-1][j-1] + 1
+      // 否则未匹配的话需要重新计算匹配数  
+      } else {
+        lcsArr[i][j] = 0
+      }
+
+      // 获取最大的匹配数和匹配的最终位置
+      if(max < lcsArr[i][j]) {
+        max = lcsArr[i][j]
+        index = j
+      }
+    }
+  }
+
+  let str = ''
+  if(max === 0) {
+    return ''
+  } else {
+    // 获取匹配的字符串
+    for(let i = index - max; i<=max; i++) {
+      str += word2[i]
+    }
+  }
+
+  return str
+}
+
+
+// [0, 0, 0, 0, 0, 0, 0, 0, 0]
+// [0, 0, 1, 0, 0, 0, 0, 0, 0]
+// [0, 0, 0, 2, 1, 1, 1, 1, 0]
+// [0, 0, 0, 1, 3, 2, 2, 2, 0]
+// [0, 0, 0, 1, 2, 4, 3, 3, 0]
+// [0, 0, 0, 1, 2, 3, 5, 4, 0]
+// [0, 0, 0, 0, 0, 0, 0, 0, 5]
+// [0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+// 查看斜角的规律
+
+lcs('a1111bc', 'ca11111b')
